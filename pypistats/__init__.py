1663fa92d49dddd8f3f600a72f27c427f68cc27c
Hugo <hugovk@users.noreply.github.com>
2018-09-24 12:10:17 +0300

    No need to instantiate a class, no internal state at the moment

--- pypistats/__init__.py
+++ pypistats/__init__.py
@@ -4,92 +4,96 @@
 Python interface to PyPI Stats API
 https://pypistats.org/api
 """
-import requests
 import pytablewriter
+import requests
 
 from . import version
 
 __version__ = version.__version__
 
+BASE_URL = "https://pypistats.org/api/"
 
-class PyPiStats(object):
-    """Python interface to PyPI Stats API"""
 
-    def __init__(self):
-        self.base_url = "https://pypistats.org/api/"
+def pypi_stats_api(endpoint, params=None, output="table"):
+    """Call the API and return JSON"""
+    if params:
+        params = "?" + params
+    else:
+        params = ""
+    url = BASE_URL + endpoint + params
 
-    def pypi_stats_api(self, endpoint, params=None, output="table"):
-        """Call the API and return JSON"""
-        if params:
-            params = "?" + params
-        else:
-            params = ""
-        url = self.base_url + endpoint + params
+    r = requests.get(url)
 
-        r = requests.get(url)
+    if r.status_code != 200:
+        return None
 
-        if r.status_code != 200:
-            return None
+    res = r.json()
 
-        if output == "json":
-            return r.json()
+    if output == "json":
+        return res
 
-        data = r.json()["data"]
-        return self._tabulate(data)
+    data = res["data"]
+    return _tabulate(data)
 
-    def _tabulate(self, data):
-        writer = pytablewriter.MarkdownTableWriter()
-        writer.margin = 1
 
-        if isinstance(data, dict):
-            writer.header_list = list(data.keys())
-            writer.value_matrix = [data]
-        elif isinstance(data, list):
-            writer.header_list = list(set().union(*(d.keys() for d in data)))
-            writer.value_matrix = data
+def _tabulate(data):
+    writer = pytablewriter.MarkdownTableWriter()
+    writer.margin = 1
 
-        return writer.dumps()
+    if isinstance(data, dict):
+        writer.header_list = list(data.keys())
+        writer.value_matrix = [data]
+    elif isinstance(data, list):
+        writer.header_list = list(set().union(*(d.keys() for d in data)))
+        writer.value_matrix = data
 
-    def _paramify(self, param_name, param_value):
-        """If param_value, append &param_name=param_value to params"""
-        if isinstance(param_value, bool):
-            param_value = str(param_value).lower()
+    return writer.dumps()
 
-        if param_value:
-            return "&" + param_name + "=" + str(param_value)
 
-        return ""
+def _paramify(param_name, param_value):
+    """If param_value, append &param_name=param_value to params"""
+    if isinstance(param_value, bool):
+        param_value = str(param_value).lower()
 
-    def recent(self, package, period=None, **kwargs):
-        """Retrieve the aggregate download quantities for the last day/week/month"""
-        endpoint = f"packages/{package}/recent"
+    if param_value:
+        return "&" + param_name + "=" + str(param_value)
 
-        params = self._paramify("period", period)
-        return self.pypi_stats_api(endpoint, params, **kwargs)
+    return ""
 
-    def overall(self, package, mirrors=None, **kwargs):
-        """Retrieve the aggregate daily download time series with or without mirror
-        downloads"""
-        endpoint = f"packages/{package}/overall"
-        params = self._paramify("mirrors", mirrors)
-        return self.pypi_stats_api(endpoint, params, **kwargs)
 
-    def python_major(self, package, version=None, **kwargs):
-        """Retrieve the aggregate daily download time series by Python major version
-        number"""
-        endpoint = f"packages/{package}/python_major"
-        params = self._paramify("version", version)
-        return self.pypi_stats_api(endpoint, params, **kwargs)
+def recent(package, period=None, **kwargs):
+    """Retrieve the aggregate download quantities for the last day/week/month"""
+    endpoint = f"packages/{package}/recent"
+    params = _paramify("period", period)
+    return pypi_stats_api(endpoint, params, **kwargs)
 
-    def python_minor(self, package, version=None, **kwargs):
-        """Retrieve the aggregate daily download time series by Python minor version
-        number"""
-        endpoint = f"packages/{package}/python_minor"
-        params = self._paramify("version", version)
-        return self.pypi_stats_api(endpoint, params, **kwargs)
 
-    def system(self, package, os=None, **kwargs):
-        """Retrieve the aggregate daily download time series by operating system"""
-        endpoint = f"packages/{package}/system"
-        params = self._paramify("os", os)
-        return self.pypi_stats_api(endpoint, params, **kwargs)
+def overall(package, mirrors=None, **kwargs):
+    """Retrieve the aggregate daily download time series with or without mirror
+    downloads"""
+    endpoint = f"packages/{package}/overall"
+    params = _paramify("mirrors", mirrors)
+    return pypi_stats_api(endpoint, params, **kwargs)
+
+
+def python_major(package, version=None, **kwargs):
+    """Retrieve the aggregate daily download time series by Python major version
+    number"""
+    endpoint = f"packages/{package}/python_major"
+    params = _paramify("version", version)
+    return pypi_stats_api(endpoint, params, **kwargs)
+
+
+def python_minor(package, version=None, **kwargs):
+    """Retrieve the aggregate daily download time series by Python minor version
+    number"""
+    endpoint = f"packages/{package}/python_minor"
+    params = _paramify("version", version)
+    return pypi_stats_api(endpoint, params, **kwargs)
+
+
+def system(package, os=None, **kwargs):
+    """Retrieve the aggregate daily download time series by operating system"""
+    endpoint = f"packages/{package}/system"
+    params = _paramify("os", os)
+    return pypi_stats_api(endpoint, params, **kwargs)
