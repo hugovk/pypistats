fbf31027119dc54052754cb75199e535fd7da28f
Hugo <hugovk@users.noreply.github.com>
2018-09-25 11:10:38 +0300

    Test tabulating

--- tests/test_pypistats.py
+++ tests/test_pypistats.py
@@ -113,6 +113,41 @@ class TestPypiStats(unittest.TestCase):
         # Assert
         self.assertEqual(param, "&version=3.7")
 
+    def test__tabulate(self):
+        # Arrange
+        data = [
+            {"category": "2.6", "date": "2018-08-15", "downloads": 51},
+            {"category": "2.7", "date": "2018-08-15", "downloads": 63749},
+            {"category": "3.2", "date": "2018-08-15", "downloads": 2},
+            {"category": "3.3", "date": "2018-08-15", "downloads": 40},
+            {"category": "3.4", "date": "2018-08-15", "downloads": 6095},
+            {"category": "3.5", "date": "2018-08-15", "downloads": 20358},
+            {"category": "3.6", "date": "2018-08-15", "downloads": 35274},
+            {"category": "3.7", "date": "2018-08-15", "downloads": 6595},
+            {"category": "3.8", "date": "2018-08-15", "downloads": 3},
+            {"category": "null", "date": "2018-08-15", "downloads": 1019},
+        ]
+        expected_output = """
+| category |    date    | downloads |
+|----------|------------|----------:|
+|      2.6 | 2018-08-15 |        51 |
+|      2.7 | 2018-08-15 |     63749 |
+|      3.2 | 2018-08-15 |         2 |
+|      3.3 | 2018-08-15 |        40 |
+|      3.4 | 2018-08-15 |      6095 |
+|      3.5 | 2018-08-15 |     20358 |
+|      3.6 | 2018-08-15 |     35274 |
+|      3.7 | 2018-08-15 |      6595 |
+|      3.8 | 2018-08-15 |         3 |
+| null     | 2018-08-15 |      1019 |
+"""
+
+        # Act
+        output = pypistats._tabulate(data)
+
+        # Assert
+        self.assertEqual(output.strip(), expected_output.strip())
+
     def test__total(self):
         # Arrange
         from data.python_minor import data
