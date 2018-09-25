474c085f5358cbe47d78f2a16c8f12cc59fff8e2
Hugo <hugovk@users.noreply.github.com>
2018-09-25 10:43:04 +0300

    Don't shadow builtin 'input'

--- tests/test_pypistats.py
+++ tests/test_pypistats.py
@@ -11,22 +11,22 @@ import pypistats
 class TestPypiStats(unittest.TestCase):
     def test__filter_no_filters_no_change(self):
         # Arrange
-        from data.python_minor import data as input
+        from data.python_minor import data
 
         # Act
-        output = pypistats._filter(input)
+        output = pypistats._filter(data)
 
         # Assert
-        self.assertEqual(input, output)
+        self.assertEqual(data, output)
 
     def test__filter_start_date(self):
         # Arrange
-        from data.python_minor import data as input
+        from data.python_minor import data
 
         start_date = "2018-09-22"
 
         # Act
-        output = pypistats._filter(input, start_date=start_date)
+        output = pypistats._filter(data, start_date=start_date)
 
         # Assert
         self.assertEqual(len(output), 20)
@@ -35,12 +35,12 @@ class TestPypiStats(unittest.TestCase):
 
     def test__filter_end_date(self):
         # Arrange
-        from data.python_minor import data as input
+        from data.python_minor import data
 
         end_date = "2018-04-22"
 
         # Act
-        output = pypistats._filter(input, end_date=end_date)
+        output = pypistats._filter(data, end_date=end_date)
 
         # Assert
         self.assertEqual(len(output), 62)
@@ -49,13 +49,13 @@ class TestPypiStats(unittest.TestCase):
 
     def test__filter_start_and_end_date(self):
         # Arrange
-        from data.python_minor import data as input
+        from data.python_minor import data
 
         start_date = "2018-09-01"
         end_date = "2018-09-11"
 
         # Act
-        output = pypistats._filter(input, start_date=start_date, end_date=end_date)
+        output = pypistats._filter(data, start_date=start_date, end_date=end_date)
 
         # Assert
         self.assertEqual(len(output), 112)
@@ -115,10 +115,10 @@ class TestPypiStats(unittest.TestCase):
 
     def test__total(self):
         # Arrange
-        from data.python_minor import data as input
+        from data.python_minor import data
 
         # Act
-        output = pypistats._total(input)
+        output = pypistats._total(data)
 
         # Assert
         self.assertEqual(len(output), 12)
